41d4c55a7f8d7a8f8346e81b1ff46b5db1c7ed08
AbdoWise-z <82497296+AbdoWise-z@users.noreply.github.com>
2023-06-27 02:00:39 +0300

    Create UVA 10305.cpp

---
+++ A/UVA 10305.cpp
@@ -0,0 +1,37 @@
+//its basiclly the same as this one , but its easier
+//this is NOT the solution of this problem , but it should
+//be something similar to this
+int arr[100000];
+
+//set the boss of j to be i
+void setBoss(int i, int j) {
+    if (arr[j] == 0) {
+        arr[j] = i + 1;
+        return;
+    }
+    setBoss(i, arr[j] - 1);
+}
+
+int main() {
+    using namespace std;
+    FAST_IO;
+
+    int n, k;
+    cin >> n >> k;
+
+    memset(arr, 0, sizeof(int) * n);
+
+    for (int i = 0; i < k; i++) {
+        int j;
+        cin >> j;
+        while (j--) {
+            int b;
+            cin >> b;
+            setBoss(i , b - 1);
+        }
+    }
+
+    for (int i = 0; i < n; i++) {
+        cout << arr[i] << endl;
+    }
+}
